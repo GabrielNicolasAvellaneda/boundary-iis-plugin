20db7627380d0884437b0d3bb43a3342c7e4cf99
gavellaneda <avellaneda.gabriel@gmail.com>
2015-02-20 17:04:46 +0000

    Added the windows meter and the split function

---
+++ modules/lib.lua
@@ -0,0 +1,18 @@
+local string = require('string')
+local table = require('table')
+
+function string:split( inSplitPattern, outResults )
+
+   if not outResults then
+      outResults = { }
+   end
+   local theStart = 1
+   local theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+   while theSplitStart do
+      table.insert( outResults, string.sub( self, theStart, theSplitStart-1 ) )
+      theStart = theSplitEnd + 1
+      theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+   end
+   table.insert( outResults, string.sub( self, theStart ) )
+   return outResults
+end
---
+++ tools/get-performance-counters.ps1
@@ -0,0 +1,3 @@
+# Simulate performance counter output
+Write-Host "241\\computador\physicaldisk(_total)\% tempo de disco|0,98878039172453"
+Write-Host "342|\\computador\physicaldisk(_total)\comprimento da fila de disco atual|0,4378039172453"
